6cc8c19b28436618fe58f3953fc86b482b868f9f
Gary <gary.gangzhou@gmail.com>
2020-03-07 08:05:32 -0500

    add spml parser

---
+++ SPML_model/Model/SPMLMessage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SPMLParser.Model
+{
+    public class SPMLMessage
+    {
+        protected XmlDocument document;
+        public string Message { get; set; }  
+
+        private string psoID;
+        public string PSOID
+        {
+            get
+            {
+                LoadXMLString();
+                if (psoID == null)
+                {
+                    psoID = GetPSOID();
+                }
+                return psoID;
+            }
+        }       
+
+        protected void LoadXMLString()
+        {
+            if (document == null)
+            {
+                document = new XmlDocument();
+                document.LoadXml(Message);
+            }
+        }
+
+        private  string GetPSOID()
+        {
+            foreach (XmlNode node in document.DocumentElement.ChildNodes)
+            {
+                if (node.Name.IndexOf("psoID", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return node.Attributes["ID"].Value;
+                }
+            }
+            //not found
+            return string.Empty;
+        }
+
+    }
+}
---
+++ SPML_model/Model/SPML_AddRequest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SPMLParser.Model
+{
+    public class SPML_AddRequest : SPMLMessage
+    {
+        private Dictionary<string, string> data = null;
+        public Dictionary<string, string> Data
+        {
+            get
+            {
+                LoadXMLString();
+
+                if (data == null)
+                    LoadData();
+
+                return data;
+            }
+        }
+
+        private void LoadData()
+        {
+            data = new Dictionary<string, string>();
+            //find data node first
+            XmlNode dataNode = null;
+            foreach (XmlNode node in document.DocumentElement.ChildNodes)
+            {
+                if (node.Name.IndexOf("data", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    dataNode = node;
+                    break;
+                }
+            }
+            if (dataNode != null)
+            {
+                foreach (XmlNode node in dataNode.ChildNodes)
+                {
+                    string key = node.Attributes["name"].Value;
+                    string val = node.FirstChild.InnerText ?? string.Empty;
+                    data.Add(key, val);
+                }
+            }
+        }
+    }
+}
